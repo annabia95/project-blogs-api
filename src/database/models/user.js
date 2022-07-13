a4cf886d8efee1d50e19270f36679bfcb217d828
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-07-13 17:20:43 -0300

    feat: :fire: add req02 - create user.js (models)

---
+++ src/database/models/user.js
@@ -0,0 +1,17 @@
+const UserSchema = (sequelize, DataTypes) => {
+  const UserTable = sequelize.define("User", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, { timestamps: false });
+
+  UserTable.associate = (models) => {
+    UserTable.hasMany(models.BlogPost, { foreignKey: "userId", as: "posts" });
+  }
+
+  return UserTable;
+}
+
+module.exports = UserSchema;
