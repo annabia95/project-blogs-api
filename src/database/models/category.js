7352975772bb645f7d5fbdf9705485e4318f9d6f
Anna Beatriz Garcia <annagarcia@id.uff.br>
2022-07-14 20:42:09 -0300

    feat: :fire: add req07 - create category.js (models)

---
+++ src/database/models/category.js
@@ -0,0 +1,10 @@
+const Categories = (sequelize, DataTypes) => {
+  const CategoriesTable = sequelize.define('Category', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    name: DataTypes.STRING,
+  }, { timestamps: false });
+
+  return CategoriesTable;
+}
+
+module.exports = Categories;
